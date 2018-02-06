a5d0b5e75c7076ebe2abdee1bb87ba5e5b83c4b6
AJWood515 <32825644+AJWood515@users.noreply.github.com>
2018-02-06 09:09:44 -0600

    StackWood
    A Stack in java

---
+++ StackWood.java
@@ -0,0 +1,33 @@
+class StackWood
+{ 
+	private int maxSize;     
+	long[] stackArray;
+	private int top;       
+
+	public StackWood(int s)         
+	{ 
+		maxSize = s;             
+		stackArray = new long[maxSize];  
+		top = -1;                
+	}	
+	public void push(long j)
+	{ 
+		stackArray[++top] = j;
+	}	
+	public long pop()
+	{ 
+		return stackArray[top--];  
+	} 
+	public long peek()
+	{ 
+		return stackArray[top]; 
+	} 
+	public boolean isEmpty()
+	{
+		return (top == -1); 
+	} 
+	public boolean isFull() 
+	{
+		return (top == maxSize-1); 
+	} 
+}
